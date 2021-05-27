f29508b38fc5e9fd80f34f6e7234a4616c8896bf
Brook1711 <670396323@qq.com>
2021-05-28 01:44:55 +0800

    save

--- RL_api/official/DDPG_agnet.py
+++ RL_api/official/DDPG_agnet.py
@@ -1,4 +1,4 @@
 import tensorflow as tf
 # this project is based on tensorflow 2.5.0
-# print("tf version:  ", tf.__version__)
-# print("is gpu avalable: ", tf.test.is_gpu_available())
+print("tf version:  ", tf.__version__)
+print("is gpu avalable: ", tf.test.is_gpu_available())
